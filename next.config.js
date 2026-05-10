fc3e8c8a7a7f0f4ec3886730c9c36bb676c3b17a
Emmanuel HAVUGIMANA <40226233+ehavugi@users.noreply.github.com>
2026-05-10 00:54:40 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,15 +2,15 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/circuitpaint',
+  assetPrefix: '/circuitpaint',
   outputFileTracingRoot: path.resolve(__dirname),
+  images: { unoptimized: true },
   webpack: (config) => {
     config.externals = [...(config.externals || []), { canvas: 'canvas' }];
     return config;
   },
-  output: 'export',
-  images: {
-    unoptimized: true, // required for static export
-  },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
